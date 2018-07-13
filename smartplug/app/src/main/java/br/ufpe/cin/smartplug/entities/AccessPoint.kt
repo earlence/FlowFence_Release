3a52e468c636e06cc0edd207ab565804559c0fd4
Davino Junior <davinomaurojr@gmail.com>
2018-07-13 16:22:29 -0300

    smart-plug app without FlowFence

---
+++ smartplug/app/src/main/java/br/ufpe/cin/smartplug/entities/AccessPoint.kt
@@ -0,0 +1,36 @@
+package br.ufpe.cin.smartplug.entities
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
+
+/*
+ * Created by Davino Junior - dmtsj@{cin.ufpe.br, gmail.com}
+ * at 07/09/2018 11:43
+ */
+
+class AccessPoint {
+
+/*  Extracted properties of TP-Link Kasa app
+        private val bssid: String? = null
+        private val channel: Int? = null
+        private val channelStatus: String? = null
+        private val cipherType: AccessPointCipherType? = null
+        private val enabled: Boolean? = null
+        private val isConnected: Boolean? = null
+        private val isSecured: Boolean? = null
+        private val keyType: AccessPointKeyType? = null
+        private val macAddress: String? = null
+        private val password: String? = null
+        private val passwordChangeFrequency: PasswordChangeFrequency? = null
+        private val rssi: Int? = null
+        private val ssid: String? = null
+        private val ssidHidden: Boolean? = null
+        private val uplinkSpeed: Int? = null
+        private val wepIndex: WepKeyIndex? = null
+        private val wepKeyFormat: String? = null
+        private val wirelessBand: String? = null
+    */
+}
